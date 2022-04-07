e14214c12c9035a0433c6eea1c58cbd14f09c9da
Fernnado <fcnogueirarp@gmail.com>
2022-04-07 19:39:25 -0300

    introdution to search

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { cards } from '../../../cards';
+import {map, startWith} from 'rxjs/operators';
+
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-toolbar',
@@ -8,7 +15,26 @@ import { cards } from '../../../cards';
 })
 export class ToolbarComponent implements OnInit {
   cards = ([] = [...cards]);
-  constructor() {}
 
-  ngOnInit(): void {}
+  myControl = new FormControl();
+  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
+  filteredOptions: Observable<User[]> | undefined;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter(name) : this.options.slice())),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
 }
